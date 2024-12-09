3d47d2ab37ab4be83f622575148379b4f4872cea
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-12-09 14:34:19 -0800

    add dropdown to select presidents (not all presidents added yet)

--- gradio_generation.py
+++ gradio_generation.py
@@ -25,10 +25,15 @@ model: PresGPT2 = PresGPT2(config)
 model.load_state_dict(checkpoint)
 model.eval()
 
+president_names = ["Abraham Lincoln", "Barack Obama", "Bill Clinton", "Donald Trump", "George Washington", 
+                   "Harry S. Truman", "Joe Biden", "John F. Kennedy", "Lyndon B. Johnson", "Richard Nixon"]
+
 # Function to generate text based on input
-def generate_text(input_text: str, generation_len: int, top_k: int, temperature: float):
+def generate_text(president: str, input_text: str, generation_len: int, top_k: int, temperature: float):
+    prompt_text = f"<President: {president}> {input_text}"
+    
     # Tokenize the input text
-    text_tokens = torch.tensor(pres_enc.encode(input_text, allowed_special='all'))
+    text_tokens = torch.tensor(pres_enc.encode(prompt_text, allowed_special='all'))
     text_tokens = torch.unsqueeze(text_tokens, 0)
 
     # Generate text
@@ -55,14 +60,15 @@ def generate_text(input_text: str, generation_len: int, top_k: int, temperature:
 # Create Gradio interface
 iface = gr.Interface(fn=generate_text, 
                      inputs=[
-                         gr.Textbox(label="Enter a prompt", lines=2),
-                         gr.Slider(minimum=10, maximum=200, value=100, label="Generation Length"),
-                         gr.Slider(minimum=1, maximum=500, value=200, label="Top-k"),
-                         gr.Slider(minimum=0.1, maximum=1.5, step=0.1, value=0.8, label="Temperature")
+                        gr.Dropdown(choices=president_names, label="Select a President"),
+                        gr.Textbox(label="Enter a prompt", lines=2),
+                        gr.Slider(minimum=10, maximum=200, value=100, label="Generation Length"),
+                        gr.Slider(minimum=1, maximum=500, value=200, label="Top-k"),
+                        gr.Slider(minimum=0.1, maximum=1.5, step=0.1, value=0.8, label="Temperature")
                      ],
                      outputs=gr.Textbox(label="Generated Text", lines=10),
                      title="PresGPT2 Text Generator",
-                     description="Enter a prompt starting with <President: Name> to generate a presidential speech.")
+                     description="Enter a prompt to generate something a president would say!")
 
 # Launch the app
 iface.launch()
